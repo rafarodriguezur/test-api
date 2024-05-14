4293c39756c8b0de812be9d80da257bd11a1092c
Rafael Rodriguez <rafael.rodriguez@cleteci.com>
2024-05-13 22:21:36 -0400

    add Port

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import pg from 'pg'
 
 const db = new pg.Pool({
     host: process.env.DB_HOST ?? 'localhost',
-    port: 3200,
+    port: process.env.DB_PORT as unknown as number ?? 5432,
     user: process.env.DB_USER ?? 'postgres',
     password: process.env.DB_PASSWORD ?? 'admin',
     database: process.env.DB_NAME ?? 'dr5_starts_youth_db',
