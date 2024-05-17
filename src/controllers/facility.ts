d9a07553d5d82f7167ce9693a529ed0f1b8657c8
Rafael Rodriguez <rafael.rodriguez@cleteci.com>
2024-05-17 09:42:52 -0400

    add paginate

--- src/controllers/facility.ts
+++ src/controllers/facility.ts
@@ -6,8 +6,9 @@ export class FacilityController {
   getAllByService = async (req: Request, res: Response) => {
     const serviceId: number = Number(req.params.id);
     const latitude: number = Number(req.query.latitude);
-    const longitude: number = Number(req.query.longitude)
-    const facilities = await FacilityModel.getAllByService(serviceId, latitude, longitude)
+    const longitude: number = Number(req.query.longitude);
+    const page: number = req.query.page ? Number(req.query.page) : 1;
+    const facilities = await FacilityModel.getAllByService(serviceId, latitude, longitude, page)
     res.json(facilities)
   }
 }
--- src/model/facility.ts
+++ src/model/facility.ts
@@ -2,22 +2,39 @@ import db from '../db';
 
 export class FacilityModel {
 
-  static async getAllByService (serviceId: number, latitude: number, longitude: number) {
-    let result = null
+  static async getAllByService (serviceId: number, latitude: number, longitude: number, page: number) {
+    let result = null;
+    let resultCount = null;
     try {
+      resultCount = await db.query(
+        `SELECT COUNT(hf.id) AS total
+         FROM health_facilities hf
+         INNER JOIN health_facilities_service_categories_links hfscl ON hf.id = hfscl.health_facility_id
+         INNER JOIN files_related_morphs frm ON hf.id = frm.related_id
+         INNER JOIN files f ON f.id = frm.file_id
+         LEFT JOIN health_facility_satisfaction_surveys hfss on hf.id = hfss.health_facility_id
+         WHERE hfscl.service_category_id = $1
+         AND frm.related_type = 'api::health-facility.health-facility';`, [serviceId]
+       )
+
       result = await db.query(
        `SELECT hf.id, hf.health_facility_name AS name, hf.address, f.url,
+          COUNT(hfss.survey_id) as opinions, AVG(hfss.answer)::numeric(10,2) as average,
           ROUND((6371000 * acos(cos(radians($1)) * cos(radians(hf.latitude)) * 
           cos(radians(hf.longitude) - radians($2)) + sin(radians($1)) * 
           sin(radians(hf.latitude))))::NUMERIC, 2) AS distance
         FROM health_facilities hf
         INNER JOIN health_facilities_service_categories_links hfscl ON hf.id = hfscl.health_facility_id
         INNER JOIN files_related_morphs frm ON hf.id = frm.related_id
-        INNER JOIN files f ON f.id = frm.file_id 
+        INNER JOIN files f ON f.id = frm.file_id
+        LEFT JOIN health_facility_satisfaction_surveys hfss on hf.id = hfss.health_facility_id
         WHERE hfscl.service_category_id = $3
         AND frm.related_type = 'api::health-facility.health-facility'
+        GROUP BY 
+          hf.id, f.url
         ORDER BY
-          distance ASC;`, [latitude, longitude, serviceId]
+          distance ASC
+        LIMIT 5 OFFSET (5 * ($4 - 1));`, [latitude, longitude, serviceId, page]
       )
     } catch (error) {
       console.log(`error: ${error}`)
@@ -26,6 +43,6 @@ export class FacilityModel {
     // no services found
     if (result?.rows.length === 0) return []
 
-    return result?.rows
+    return {total: resultCount?.rows[0].total, items: result?.rows}
   }
 }
