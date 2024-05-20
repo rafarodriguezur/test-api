0f498210bdc3b060d23930088e31f32f4da3da59
Rafael Rodriguez <rafael.rodriguez@cleteci.com>
2024-05-20 12:11:39 -0400

    update user

--- src/model/user.ts
+++ src/model/user.ts
@@ -17,17 +17,18 @@ export class UserModel {
     try {
         const userRows = await db.query(
         `select u.mac_address, u.age, u.gender, u.device_model, u.os_version
-         from users u where u.mac_address = $1;`, [deviceId]
+        from users u where u.mac_address = $1;`, [deviceId]
        )
        if (userRows?.rows.length === 0) {
         const result = await db.query(
           `INSERT INTO users(mac_address, age, gender, device_model, os_version, created_at, updated_at)
-            VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING mac_address, age, gender, device_model, os_version;`,
+          VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING mac_address, age, gender, device_model, os_version;`,
           [deviceId, age, gender, deviceModel, osVersion, new Date(), new Date()]
         )
         return result.rows[0]
        } else {
-         return userRows.rows[0]
+          await db.query(`UPDATE users SET age = $2, gender = $3, updated_at = $4 WHERE mac_address = $1`, [deviceId, age, gender, new Date()])
+          return userRows.rows[0]
        }
     } catch (e) {
       console.log('error', e)
