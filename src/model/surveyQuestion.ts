b5448738210d59e97433384ab1090b9bcbc0ca8c
Rafael Rodriguez <rafael.rodriguez@cleteci.com>
2024-05-30 09:09:22 -0400

    add id

--- src/model/surveyQuestion.ts
+++ src/model/surveyQuestion.ts
@@ -6,7 +6,7 @@ export class SurveyQuestionModel {
     let result = null
     try {
       result = await db.query(
-       `SELECT hfssq.question_text
+       `SELECT hfssq.id, hfssq.question_text
         FROM health_facility_satisfaction_survey_questions hfssq
         ORDER BY hfssq.question_text ;`
       )
