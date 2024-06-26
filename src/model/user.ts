fe9864dd702770208ab9cd711e5c021aed2f2c5f
Rafael Rodriguez <rafael.rodriguez@cleteci.com>
2024-06-26 13:08:35 -0400

    add is blocked

--- src/model/user.ts
+++ src/model/user.ts
@@ -12,22 +12,20 @@ export class UserModel {
       osVersion
     } = input
 
-    console.log('deviceId', deviceId)
-
     try {
         const userRows = await db.query(
-        `select u.mac_address, u.age, u.gender, u.device_model, u.os_version
+        `select u.mac_address, u.age, u.gender, u.device_model, u.os_version, u.is_blocked
         from users u where u.mac_address = $1;`, [deviceId]
        )
        if (userRows?.rows.length === 0) {
         const result = await db.query(
-          `INSERT INTO users(mac_address, age, gender, device_model, os_version, created_at, updated_at)
-          VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING mac_address, age, gender, device_model, os_version;`,
-          [deviceId, age, gender, deviceModel, osVersion, new Date(), new Date()]
+          `INSERT INTO users(mac_address, age, gender, device_model, os_version)
+          VALUES ($1, $2, $3, $4, $5) RETURNING mac_address, age, gender, device_model, os_version, is_blocked;`,
+          [deviceId, age, gender, deviceModel, osVersion]
         )
         return result.rows[0]
        } else {
-          await db.query(`UPDATE users SET age = $2, gender = $3, updated_at = $4 WHERE mac_address = $1`, [deviceId, age, gender, new Date()])
+          await db.query(`UPDATE users SET age = $2, gender = $3, updated_at = CURRENT_TIMESTAMP WHERE mac_address = $1`, [deviceId, age, gender])
           return userRows.rows[0]
        }
     } catch (e) {
@@ -39,10 +37,19 @@ export class UserModel {
   static async updateToken(input: any) {
 
     const {token, deviceId } = input;
-
+    
     try {
-        const userRows = await db.query(`UPDATE users SET token = $2, updated_at = $3 WHERE mac_address = $1 RETURNING mac_address;`, [deviceId, token, new Date()])
+      const findUserRows = await db.query(
+        `select u.mac_address
+        from users u where u.mac_address = $1;`, [deviceId]
+       )
+       if (findUserRows.rows.length > 0) {
+        const userRows = await db.query(`UPDATE users SET token = $2, updated_at = CURRENT_TIMESTAMP WHERE mac_address = $1 RETURNING mac_address;`, [deviceId, token])
         return userRows.rows[0]
+       } else {
+        return {error: true}
+       }
+      
     } catch (e) {
       console.log('error', e)
       return {error: e}
